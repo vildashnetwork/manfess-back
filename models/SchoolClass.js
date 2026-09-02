27a84f7180bb75fe44f588f00b81e3545007412e
VILDASH NETWORK <vildashnetwork02@gmail.com>
2026-09-02 05:05:43 +0100

    first verion

--- models/SchoolClass.js
+++ models/SchoolClass.js
@@ -4,7 +4,7 @@ import { syncTombstonePlugin } from "../db/syncPlugin.js";
 const schoolClassSchema = new mongoose.Schema({
     className: {
         type: String,
-        enum: ["Beginers", "level 3", "level 4", "level 5", "Upper 6th", "Graduated"],
+        enum: ["Beginers", "level 3", "level 4", "level 5", "Alevel", "Graduated"],
         required: [true, "Class name is required"],
         trim: true
     },
